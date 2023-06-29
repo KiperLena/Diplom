b595d07a40d105298e444e0f826d9cbaad863590
KiperLena <cherkaschina_ea@mail.ru>
2023-06-29 11:42:05 +0500

    diplom

--- SPM/centre/views.py
+++ SPM/centre/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Area
-from django.http import FileResponse, Http404
+
 
 def index(request):
     return render(request, 'centre/index.html')
@@ -21,7 +21,3 @@ def area(request, pk):
 
 def old_doc(request):
     return render(request, 'centre/biblioteka.html')
-    # try:
-    #     return FileResponse(open('foobar.pdf', 'rb'), content_type='application/pdf')
-    # except FileNotFoundError:
-    #     raise Http404()
