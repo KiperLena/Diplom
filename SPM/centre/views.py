b8cebedec5218c47d823e642f232f3b0e58cbf14
KiperLena <cherkaschina_ea@mail.ru>
2023-08-31 14:43:09 +0500

    форма регистрации окончательный редакт

--- SPM/centre/views.py
+++ SPM/centre/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Area, Department1, Group, Department2,Directorate
 from .forms import ReportForm
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 def index(request):
@@ -20,15 +21,31 @@ def index(request):
 
 def centre(request):
     license_area = Area.objects.all()
+    paginator = Paginator(license_area, 24)
+    page = request.GET.get('page')
+
+    try:
+        license_area = paginator.page(page)
+    except PageNotAnInteger:
+        # Если страница не является целым числом, поставим первую страницу
+        license_area = paginator.page(1)
+    except EmptyPage:
+        # Если страница больше максимальной, доставить последнюю страницу результатов
+        license_area = paginator.page(paginator.num_pages)
+
     context = {
+        'page': page,
         'area': license_area,
+        'paginator': paginator,
     }
     return render(request, 'centre/centre.html', context)
 
 
 def area(request, pk):
+
     licenses = Area.objects.get(id=pk)
-    return render(request, 'centre/license_area.html', {'license': licenses})
+
+    return render(request, 'centre/area.html')
 
 
 def old_doc(request):
--- SPM/users/forms.py
+++ SPM/users/forms.py
@@ -3,9 +3,15 @@ from django.contrib.auth.models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'fadeIn third'
+
+
     class Meta:
         model = User #какой класс подключила
-        fields = ['first_name', 'last_name', 'username', 'password1', 'password2'] #поля которые подключаем
+        fields = ['first_name', 'last_name', 'username'] #поля которые подключаем
         labels = {
             'first_name': 'Введите Имя',
             'last_name': 'Введите Фамилию',
